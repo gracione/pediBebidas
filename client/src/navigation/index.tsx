4b01118f62853f518dd049b0d83ec016158b595e
Gracione Silva <gracione2017@gmail.com>
2024-02-01 18:40:44 -0300

    [pasta LoginScreen]

--- client/src/navigation/index.tsx
+++ client/src/navigation/index.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { HomeScreen } from '../screens/HomeScreen';
+import {HomeScreen} from '../screens/HomeScreen';
+import {LoginScreen} from '../screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
-
 export function Navigation() {
+    //temporario até começar a regra de login
+    if (true) {
+        return (
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Home" component={LoginScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
+      );
+    }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
