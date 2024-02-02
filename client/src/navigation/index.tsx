ce581f11ffe4958af6a7890feb3b1571284084d9
Gracione Silva <gracione2017@gmail.com>
2024-02-02 06:58:03 -0300

    [iniciando login]

--- client/src/navigation/index.tsx
+++ client/src/navigation/index.tsx
@@ -1,22 +1,22 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {HomeScreen} from '../screens/HomeScreen';
-import {LoginScreen} from '../screens/LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { LoginScreen } from '../screens/LoginScreen';
+import { HomeScreen } from '../screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
 export function Navigation() {
-    //temporario até começar a regra de login
-    if (true) {
-        return (
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen name="Home" component={LoginScreen} />
-                </Stack.Navigator>
-            </NavigationContainer>
-      );
-    }
+  //temporario até começar a regra de login
+  if (true) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 
   return (
     <NavigationContainer>
--- client/src/screens/LoginScreen.tsx
+++ client/src/screens/LoginScreen.tsx
@@ -1,9 +1,29 @@
-import {View, Text} from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
-export function LoginScreen() {
-    return (
-        <View>
-            <Text>22</Text>
-        </View>
-    )
-}
+export function LoginScreen () {
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Lógica de autenticação aqui
+    // Se a autenticação for bem-sucedida, navegue para a tela Home
+    // navigation.navigate('Home');
+  };
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Username"
+      />
+      <TextInput
+        placeholder="Password"
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+export default LoginScreen;
