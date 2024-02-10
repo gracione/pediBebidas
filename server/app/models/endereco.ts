bd1664d91be2258b51fd36cd51b610e358638f7f
Gracione Silva <gracione2017@gmail.com>
2024-02-10 08:59:43 -0300

    [coulnas do endereco]

--- server/app/models/endereco.ts
+++ server/app/models/endereco.ts
@@ -1,9 +1,26 @@
 import { BaseModel, column } from '@adonisjs/lucid/orm'
 
 export default class Endereco extends BaseModel {
-  public static table = 'endereco';
+  public static table = 'endereco'
 
   @column({ isPrimary: true })
   declare id: number
 
+  @column()
+  declare rua: string
+
+  @column()
+  declare numero: number
+
+  @column()
+  declare bairro: string
+
+  @column()
+  declare complementar: string
+
+  @column()
+  declare latitude: number
+
+  @column()
+  declare longitude: number
 }
