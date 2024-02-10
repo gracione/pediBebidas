d1d69adf6a0e8425b7900087f16e9b2290707aab
Gracione Silva <gracione2017@gmail.com>
2024-02-10 08:43:25 -0300

    [configuração banco postgres]

--- server/app/models/endereco.ts
+++ server/app/models/endereco.ts
@@ -1,13 +1,9 @@
-import { DateTime } from 'luxon'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
 
 export default class Endereco extends BaseModel {
+  public static table = 'endereco';
+
   @column({ isPrimary: true })
   declare id: number
 
-  @column.dateTime({ autoCreate: true })
-  declare createdAt: DateTime
-
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  declare updatedAt: DateTime
 }
