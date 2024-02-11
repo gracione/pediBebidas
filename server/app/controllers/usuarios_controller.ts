349bb1303e45a138d10d96692320e25cdf4654a9
Gracione Silva <gracione2017@gmail.com>
2024-02-11 14:25:24 -0300

    [identacao]

--- server/app/controllers/usuarios_controller.ts
+++ server/app/controllers/usuarios_controller.ts
@@ -1,6 +1,6 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import Usuario from '#models/usuario'
-import Endereco from '#models/endereco';
+import Endereco from '#models/endereco'
 import BaseController from './bases_controller.js'
 import { ValidateCreate, ValidateAutenticacao } from '#validators/usuario'
 import hash from '@adonisjs/core/services/hash'
@@ -8,25 +8,39 @@ import hash from '@adonisjs/core/services/hash'
 export default class UsuariosController extends BaseController {
   constructor() {
     super(Usuario)
-
   }
 
   async store({ request, response }: HttpContext) {
-    const data = request.all()
-    await ValidateCreate.validate(data)
-    const { email } = request.only(['email', 'password'])
-    const user = await Usuario.findBy('email', email)
-    
-    if (user) {
-        response.abort('Email já cadastrado')
+    try {
+      const data = request.all()
+      await ValidateCreate.validate(data)
+
+      const { email } = request.only(['email'])
+      let user = await Usuario.findBy('email', email)
+
+      if (user) {
+        return response.status(400).send({ error: 'Email já cadastrado' })
+      }
+
+      const usuarioData = request.only(['nome', 'telefone', 'email', 'password', 'data_nascimento'])
+      const enderecoData = request.only([
+        'rua',
+        'numero',
+        'bairro',
+        'complementar',
+        'latitude',
+        'longitude',
+      ])
+      const endereco = await Endereco.create(enderecoData)
+      usuarioData.id_endereco = endereco.id
+
+      user = await Usuario.create(usuarioData)
+      const token = await Usuario.accessTokens.create(user)
+
+      return response.status(201).send(token)
+    } catch (error) {
+      return response.status(500).send({ error: 'Erro interno do servidor' })
     }
-
-    const { id: id_endereco } = await Endereco.create(request.only(['rua','numero','bairro','complementar','latitude','longitude']));
-
-    data.id_endereco = id_endereco
-    const instance = await Usuario.create(request.only(['nome','telefone','email','password','data_nascimento','id_endereco']))
-    const token = await Usuario.accessTokens.create(instance)
-    return response.created(token)
   }
 
   async autenticarUsuario({ request }: HttpContext) {
