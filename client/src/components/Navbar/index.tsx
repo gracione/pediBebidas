fd95d138a5a289191fee99fbe5e358dba85d615d
Gracione Silva <gracione2017@gmail.com>
2024-02-28 20:51:08 -0300

    adicionando menu

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import {Drawer} from 'react-native-paper';
 
 interface NavbarProps {
   navigation: NativeStackNavigationProp<any>;
@@ -8,64 +9,32 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ navigation, sairDoPerfil }) => {
   const [expanded, setExpanded] = useState(false);
-
+  const [active, setActive] = React.useState('');
   const toggleMenu = () => {
     setExpanded(!expanded);
   };
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={toggleMenu} style={styles.menuButton}>
-        <Text style={styles.menuButtonText}>Menu</Text>
+    <View>
+      <TouchableOpacity onPress={toggleMenu}>
+        <Text>Menu</Text>
       </TouchableOpacity>
       {expanded && (
-        <View style={styles.menu}>
-          <TouchableOpacity onPress={() => navigation.navigate('Pedidos')} style={styles.menuItem}>
-            <Text style={styles.menuItemText}>Pedidos</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate('Perfil')} style={styles.menuItem}>
-            <Text style={styles.menuItemText}>Perfil</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate('RegisterScreen')} style={styles.menuItem}>
-            <Text style={styles.menuItemText}>Fale Conosco</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => sairDoPerfil()} style={styles.menuItem}>
-            <Text style={styles.menuItemText}>Sair</Text>
-          </TouchableOpacity>
-        </View>
+        <Drawer.Section title="Some title">
+          <Drawer.Item
+            label="First Item"
+            active={active === 'first'}
+            onPress={() => setActive('first')}
+          />
+          <Drawer.Item
+            label="Sair"
+            active={active === 'second'}
+            onPress={() => sairDoPerfil()}
+          />
+        </Drawer.Section>
       )}
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    paddingRight: 10,
-  },
-  menuButton: {
-    paddingHorizontal: 10,
-  },
-  menuButtonText: {
-    fontSize: 18,
-  },
-  menu: {
-    position: 'absolute',
-    top: 50,
-    right: 10,
-    backgroundColor: '#fff',
-    padding: 10,
-    borderRadius: 5,
-    elevation: 3,
-  },
-  menuItem: {
-    paddingVertical: 5,
-  },
-  menuItemText: {
-    fontSize: 16,
-  },
-});
 
 export default Navbar;
