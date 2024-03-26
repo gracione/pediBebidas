4987fb1adcee3f7808ccafb8b91c5d7dc948a8b3
ipeweb <gracione2017@gmail.com>
2024-03-26 02:53:47 -0300

    adicionando campos de adicionar produto

--- client/src/pages/CadastrarProduto/index.tsx
+++ client/src/pages/CadastrarProduto/index.tsx
@@ -1,14 +1,95 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Alert } from 'react-native';
+import { SelectList } from 'react-native-dropdown-select-list';
+import { Input, Button } from 'react-native-elements';
+import api from "../../service/api";
 
-const CadastrarProduto: React.FC= () => {
+interface Estabelecimento {
+  id: number;
+  nome: string;
+}
+
+const CadastrarProduto: React.FC = () => {
+  const [nome, setNome] = useState('');
+  const [valor, setValor] = useState('');
+  const [estabelecimentos, setEstabelecimentos] = useState<Estabelecimento[]>([]);
+  const [selectedEstabelecimento, setSelectedEstabelecimento] = useState<number | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.get<Estabelecimento[]>("estabelecimento");
+        setEstabelecimentos(response.data);
+      } catch (error) {
+        console.error(error);
+        Alert.alert(
+          "Erro ao carregar estabelecimentos. Por favor, tente novamente mais tarde."
+        );
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleCadastro = () => {
+    if (!nome || !valor || !selectedEstabelecimento) {
+      Alert.alert("Por favor, preencha todos os campos.");
+      return;
+    }
+    console.log("Nome:", nome);
+    console.log("Valor:", valor);
+    console.log("Estabelecimento selecionado:", selectedEstabelecimento);
+  };
+
   return (
-    <View style={{flex: 1}}>
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>CadastrarProduto</Text>
-      </View>
+    <View style={styles.container}>
+      <Text style={styles.title}>Cadastrar Produto</Text>
+      <Input
+        placeholder="Nome"
+        value={nome}
+        onChangeText={(text) => setNome(text)}
+        style={styles.input}
+      />
+      <Input
+        placeholder="Valor"
+        value={valor}
+        onChangeText={(text) => setValor(text)}
+        style={styles.input}
+      />
+      <SelectList 
+        setSelected={(val) => setSelectedEstabelecimento(val as number)}
+        data={estabelecimentos.map(estab => ({ key: estab.id, value: estab.nome }))}
+        save="value"
+        style={styles.input}
+      />
+      <Button
+        title="Cadastrar"
+        onPress={handleCadastro}
+        buttonStyle={styles.button}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    marginBottom: 10,
+  },
+  button: {
+    marginTop: 20,
+    width: '100%',
+  },
+});
 
 export default CadastrarProduto;
--- client/src/screens/LoginScreen.tsx
+++ client/src/screens/LoginScreen.tsx
@@ -20,7 +20,12 @@ const LoginScreen: React.FC<LoginScreenProps> = ({
   const [error, setError] = useState<string | null>(null);
   const authContext = React.useContext(AuthContext);
 
+  // api.defaults.headers.common['Authorization'] = `Bearer oat_MjE0.bFF4cFRqSHJaTlFaRmM3bW5BazFKWFBrdXNIQ2ZFQXZILXF5cU84OTEzNTgyMDYxMzY`;
+  // authContext.setTipoUsuario(1)
+
   const handleLogin = async (email: string, password: string) => {
+    email="pedro3@gmail.com";
+    password="123abc";
     setError(null);
 
     try {
@@ -45,7 +50,7 @@ const LoginScreen: React.FC<LoginScreenProps> = ({
       }
     }
   };
-
+  // handleLogin('pedro3@gmail.com','123abc');
   const handleRegistrar = () => {
     navigation.navigate('Cadastrar Usuario');
   };
