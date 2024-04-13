b6e0a3ed9b052244b24ab07800cb07023d36cc89
ipeweb <gracione2017@gmail.com>
2024-04-13 06:59:09 -0300

    prettierc

--- client/src/pages/Home/Estabelecimentos/index.tsx
+++ client/src/pages/Home/Estabelecimentos/index.tsx
@@ -1,96 +1,86 @@
-import React, { useState, useEffect } from "react";
-import { View, TouchableOpacity, ScrollView, Alert } from "react-native";
-import InputSearch from "../../../components/InputSearch";
-import api from "../../../service/api";
-import { Container, CardEstabelecimento, CardContent, CardText, CardImage, CardAberto, CardFechado, CardDistancia } from "./style";
+import React, { useState, useEffect } from 'react';
+import { View, TouchableOpacity, ScrollView, Alert } from 'react-native';
+import InputSearch from '../../../components/InputSearch';
+import api from '../../../service/api';
+import { Container, CardEstabelecimento, CardContent, CardText, CardImage, CardAberto, CardFechado, CardDistancia } from './style';
 import * as Location from 'expo-location';
 
 interface NavbarProps {
-  navigation: any;
-  setIdEstabelecimento: any;
+    navigation: any;
+    setIdEstabelecimento: any;
 }
 
 interface Estabelecimento {
-  id: number;
-  nome: string;
+    id: number;
+    nome: string;
 }
 
-export const Estabelecimentos: React.FC<NavbarProps> = ({
-    navigation,
-    setIdEstabelecimento,
-  }) => {
-    const [searchQuery, setSearchQuery] = useState("");
+export const Estabelecimentos: React.FC<NavbarProps> = ({ navigation, setIdEstabelecimento }) => {
+    const [searchQuery, setSearchQuery] = useState('');
     const [estabelecimentos, setEstabelecimentos] = useState<Estabelecimento[]>([]);
     const [location, setLocation] = useState<Location.LocationObject | null>(null);
     const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
     useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await api.get("estabelecimento");
-          setEstabelecimentos(response.data);
-        } catch (error) {
-          console.error(error);
-          Alert.alert(
-            "Erro ao carregar endereço. Por favor, tente novamente mais tarde."
-          );
-        }
-      };
+        const fetchData = async () => {
+            try {
+                const response = await api.get('estabelecimento');
+                setEstabelecimentos(response.data);
+            } catch (error) {
+                console.error(error);
+                Alert.alert('Erro ao carregar endereço. Por favor, tente novamente mais tarde.');
+            }
+        };
 
-      const getLocation = async () => {
-        try {
-          const { status } = await Location.requestForegroundPermissionsAsync();
-          if (status !== 'granted') {
-            setErrorMsg('Permissão para acessar a localização foi negada.');
-            return;
-          }
-        
-          const location = await Location.getCurrentPositionAsync({});
-          console.log(location);
-          setLocation(location);
-        } catch (error) {
-          setErrorMsg('Erro ao obter a localização: ' + (error as Error).message);
-        }
-      };  
-  
-      console.log(location);
-      getLocation();
-      fetchData();
+        const getLocation = async () => {
+            try {
+                const { status } = await Location.requestForegroundPermissionsAsync();
+                if (status !== 'granted') {
+                    setErrorMsg('Permissão para acessar a localização foi negada.');
+                    return;
+                }
+
+                const location = await Location.getCurrentPositionAsync({});
+                console.log(location);
+                setLocation(location);
+            } catch (error) {
+                setErrorMsg('Erro ao obter a localização: ' + (error as Error).message);
+            }
+        };
+
+        console.log(location);
+        getLocation();
+        fetchData();
     }, []);
-  
+
     const handleSearch = (query: string) => {
-      setSearchQuery(query);
+        setSearchQuery(query);
     };
     function navegar(idEstabelecimento: number) {
-      setIdEstabelecimento(idEstabelecimento);
-      navigation.navigate("FazerPedido", { id: idEstabelecimento });
+        setIdEstabelecimento(idEstabelecimento);
+        navigation.navigate('FazerPedido', { id: idEstabelecimento });
     }
     return (
-      <Container>
-        <InputSearch
-          placeholder="Pesquisar"
-          value={searchQuery}
-          onChangeText={handleSearch}
-        />
-        <ScrollView>
-          {estabelecimentos.map((estabelecimento) => (
-            <TouchableOpacity
-              key={estabelecimento.id}
-              onPress={() => {
-                navegar(estabelecimento.id);
-              }}
-            >
-              <CardEstabelecimento>
-                <CardImage url={estabelecimento.url ?? ""} />
-                <CardContent>
-                  <CardText>{estabelecimento.nome}</CardText>
-                  <CardDistancia>7 km</CardDistancia>
-                  <CardAberto>Aberto</CardAberto>
-                </CardContent>
-              </CardEstabelecimento>
-            </TouchableOpacity>
-          ))}
-        </ScrollView>
-      </Container>
+        <Container>
+            <InputSearch placeholder="Pesquisar" value={searchQuery} onChangeText={handleSearch} />
+            <ScrollView>
+                {estabelecimentos.map((estabelecimento) => (
+                    <TouchableOpacity
+                        key={estabelecimento.id}
+                        onPress={() => {
+                            navegar(estabelecimento.id);
+                        }}>
+                        <CardEstabelecimento>
+                            <CardImage url={estabelecimento.url ?? ''} />
+                            <CardContent>
+                                <CardText>{estabelecimento.nome}</CardText>
+                                <CardDistancia>7 km</CardDistancia>
+                                <CardAberto>Aberto</CardAberto>
+                            </CardContent>
+                        </CardEstabelecimento>
+                    </TouchableOpacity>
+                ))}
+            </ScrollView>
+        </Container>
     );
-  };
+};
--- client/src/pages/Perfil/MeusEstabelecimentos/index.tsx
+++ client/src/pages/Perfil/MeusEstabelecimentos/index.tsx
@@ -1,166 +1,173 @@
-import React, { useState, useEffect } from "react";
-import { Button, Alert, Text } from "react-native";
-import { StyleSheet, View } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import { EstabelecimentoComEndereco } from "./types";
-import { fetchEstabelecimentos, saveEstabelecimento } from "./api";
-import { Container, FormContainer, StyledTextInput, Card } from "./style";
-import MapView, { Marker, Region } from "react-native-maps";
+import React, {useState, useEffect} from 'react';
+import {Button, Alert, Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
+import {EstabelecimentoComEndereco} from './types';
+import {fetchEstabelecimentos, saveEstabelecimento} from './api';
+import {Container, FormContainer, StyledTextInput, Card} from './style';
+import MapView, {Marker, Region} from 'react-native-maps';
 
 interface Location {
-  latitude: number;
-  longitude: number;
+    latitude: number;
+    longitude: number;
 }
 
-
 const MeusEstabelecimentos: React.FC = () => {
-    const [estabelecimento, setEstabelecimento] = useState<EstabelecimentoComEndereco>({
-      nome: "",
-      rua: "",
-      numero: "",
-      bairro: "",
-      complementar: "",
-      latitude: "",
-      longitude: "",
-    });
-    
-  const [response, setResponse] = useState<EstabelecimentoComEndereco[]>([]);
-  const [addEndereco, setAddEndereco] = useState(false);
+    const [estabelecimento, setEstabelecimento] =
+        useState<EstabelecimentoComEndereco>({
+            nome: '',
+            rua: '',
+            numero: '',
+            bairro: '',
+            complementar: '',
+            latitude: '',
+            longitude: '',
+        });
 
-  useEffect(() => {
-    fetchData();
-  }, [addEndereco]);
+    const [response, setResponse] = useState<EstabelecimentoComEndereco[]>([]);
+    const [addEndereco, setAddEndereco] = useState(false);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetchEstabelecimentos();
-      setResponse(response);
-    } catch (error) {
-      console.error(error);
-      Alert.alert(
-        "Erro ao carregar endereço. Por favor, tente novamente mais tarde."
-      );
-    }
-  };
+    useEffect(() => {
+        fetchData();
+    }, [addEndereco]);
 
-  const handleChange = (field: keyof EstabelecimentoComEndereco, value: string) => {
-    setEstabelecimento({ ...estabelecimento, [field]: value });
-  };
+    const fetchData = async () => {
+        try {
+            const response = await fetchEstabelecimentos();
+            setResponse(response);
+        } catch (error) {
+            console.error(error);
+            Alert.alert(
+                'Erro ao carregar endereço. Por favor, tente novamente mais tarde.'
+            );
+        }
+    };
 
-  const handleSave = async () => {
-    try {
-      await saveEstabelecimento(estabelecimento);
-      Alert.alert("Endereço salvo com sucesso!");
-      setAddEndereco(false);
-      setEstabelecimento({
-        nome: "",
-        rua: "",
-        numero: "",
-        bairro: "",
-        complementar: "",
-        latitude: "",
-        longitude: "",
-      });
-    } catch (error) {
-      handleError(error);
-    }
-  };
+    const handleChange = (
+        field: keyof EstabelecimentoComEndereco,
+        value: string
+    ) => {
+        setEstabelecimento({...estabelecimento, [field]: value});
+    };
 
-  const handleError = (error: any) => {
-    if (error.response && error.response.data && error.response.data.errors) {
-      const errorsMessages = error.response.data.errors;
-      let messages = "";
-      errorsMessages.forEach((element: any) => {
-        messages += element.message + "\n";
-      });
-      Alert.alert(messages);
-    } else {
-      console.log(error);
-      Alert.alert(
-        "Erro ao salvar endereço. Por favor, tente novamente mais tarde."
-      );
-    }
-  };
+    const handleSave = async () => {
+        try {
+            await saveEstabelecimento(estabelecimento);
+            Alert.alert('Endereço salvo com sucesso!');
+            setAddEndereco(false);
+            setEstabelecimento({
+                nome: '',
+                rua: '',
+                numero: '',
+                bairro: '',
+                complementar: '',
+                latitude: '',
+                longitude: '',
+            });
+        } catch (error) {
+            handleError(error);
+        }
+    };
 
-  const handleMapPress = (event: {
-    nativeEvent: { coordinate: { latitude: number; longitude: number } };
-  }) => {
-    const { latitude, longitude } = event.nativeEvent.coordinate;
-    setEstabelecimento(prevEstabelecimento => ({
-      ...prevEstabelecimento,
-      latitude: latitude.toString(),
-      longitude: longitude.toString()
-    }));
-  };
-  
-  return (
-    <Container>
-      {addEndereco ? (
-        <FormContainer>
-          <StyledTextInput
-            onChangeText={(text) => handleChange("nome", text)}
-            placeholder="Nome"
-          />
-          <StyledTextInput
-            onChangeText={(text) => handleChange("rua", text)}
-            placeholder="Rua"
-          />
-          <StyledTextInput
-            onChangeText={(text) => handleChange("numero", text)}
-            placeholder="Número"
-            keyboardType="numeric"
-          />
-          <StyledTextInput
-            onChangeText={(text) => handleChange("bairro", text)}
-            placeholder="Bairro"
-          />
-          <StyledTextInput
-            onChangeText={(text) => handleChange("complementar", text)}
-            placeholder="Complemento"
-          />
-          <Text>Escolha a localização</Text>
-          <MapView
-            style={styles.map}
-            initialRegion={{
-              latitude: -17.3920936,
-              longitude: -50.373832,
-              latitudeDelta: 0.01,
-              longitudeDelta: 0.01,
-            }}
-            onPress={handleMapPress}
-          >
-          </MapView>
+    const handleError = (error: any) => {
+        if (
+            error.response &&
+            error.response.data &&
+            error.response.data.errors
+        ) {
+            const errorsMessages = error.response.data.errors;
+            let messages = '';
+            errorsMessages.forEach((element: any) => {
+                messages += element.message + '\n';
+            });
+            Alert.alert(messages);
+        } else {
+            console.log(error);
+            Alert.alert(
+                'Erro ao salvar endereço. Por favor, tente novamente mais tarde.'
+            );
+        }
+    };
 
-          <Button title="Salvar" onPress={handleSave} />
-        </FormContainer>
-      ) : (
-        <>
-          <Ionicons
-            name="add-circle-outline"
-            size={24}
-            color="black"
-            onPress={() => setAddEndereco(true)}
-          />
+    const handleMapPress = (event: {
+        nativeEvent: {coordinate: {latitude: number; longitude: number}};
+    }) => {
+        const {latitude, longitude} = event.nativeEvent.coordinate;
+        setEstabelecimento((prevEstabelecimento) => ({
+            ...prevEstabelecimento,
+            latitude: latitude.toString(),
+            longitude: longitude.toString(),
+        }));
+    };
 
-          {response.map((estabelecimentoF, index) => (
-            <Card key={index}>
-              <Text>{estabelecimentoF.nome}</Text>
-            </Card>
-          ))}
-        </>
-      )}
-    </Container>
-  );
+    return (
+        <Container>
+            {addEndereco ? (
+                <FormContainer>
+                    <StyledTextInput
+                        onChangeText={(text) => handleChange('nome', text)}
+                        placeholder="Nome"
+                    />
+                    <StyledTextInput
+                        onChangeText={(text) => handleChange('rua', text)}
+                        placeholder="Rua"
+                    />
+                    <StyledTextInput
+                        onChangeText={(text) => handleChange('numero', text)}
+                        placeholder="Número"
+                        keyboardType="numeric"
+                    />
+                    <StyledTextInput
+                        onChangeText={(text) => handleChange('bairro', text)}
+                        placeholder="Bairro"
+                    />
+                    <StyledTextInput
+                        onChangeText={(text) =>
+                            handleChange('complementar', text)
+                        }
+                        placeholder="Complemento"
+                    />
+                    <Text>Escolha a localização</Text>
+                    <MapView
+                        style={styles.map}
+                        initialRegion={{
+                            latitude: -17.3920936,
+                            longitude: -50.373832,
+                            latitudeDelta: 0.01,
+                            longitudeDelta: 0.01,
+                        }}
+                        onPress={handleMapPress}></MapView>
+
+                    <Button title="Salvar" onPress={handleSave} />
+                </FormContainer>
+            ) : (
+                <>
+                    <Ionicons
+                        name="add-circle-outline"
+                        size={24}
+                        color="black"
+                        onPress={() => setAddEndereco(true)}
+                    />
+
+                    {response.map((estabelecimentoF, index) => (
+                        <Card key={index}>
+                            <Text>{estabelecimentoF.nome}</Text>
+                        </Card>
+                    ))}
+                </>
+            )}
+        </Container>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: "100%",
-    height: "30%",
-  },
+    container: {
+        flex: 1,
+    },
+    map: {
+        width: '100%',
+        height: '30%',
+    },
 });
 
 export default MeusEstabelecimentos;
