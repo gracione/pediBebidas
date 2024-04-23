a1a278ff539fbfba104c61c9be613f6778b9d6a5
ipeweb <gracione2017@gmail.com>
2024-04-23 03:23:27 -0300

    buscar dados do estabelecimento

--- client/src/pages/Perfil/MeusEstabelecimentos/EditarEstabelecimento/index.tsx
+++ client/src/pages/Perfil/MeusEstabelecimentos/EditarEstabelecimento/index.tsx
@@ -1,10 +1,43 @@
-import React, {useState, useEffect} from 'react';
-import {View, TextInput, StyleSheet, Button, Alert} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, StyleSheet, Button, Alert } from 'react-native';
+import { Text } from 'react-native-elements';
+import api from '../../../../service/api';
 
-export const EditarEstabelecimento: React.FC = () => {
-        return (
-            <View>
-                <TextInput></TextInput>
-            </View>
-        )
+interface Estabelecimento {
+    id: number;
+    nome: string;
 }
+
+interface EditarEstabelecimentoProps {
+    route: {
+        params: {
+            id: string;
+        };
+    };
+}
+
+export const EditarEstabelecimento: React.FC<EditarEstabelecimentoProps> = ({ route }) => {
+    const { id } = route.params;
+    const [estabelecimento, setEstabelecimento] = useState<Estabelecimento | null>(null);
+
+    useEffect(() => {
+        const fetchEstabelecimento = async () => {
+            try {
+                const response = await api.get(`estabelecimento/${id}`);
+                setEstabelecimento(response.data);
+            } catch (error) {
+                console.error('Erro ao buscar estabelecimento:', error);
+                Alert.alert('Erro', 'Ocorreu um erro ao buscar o estabelecimento.');
+            }
+        };
+        console.log(estabelecimento)
+        fetchEstabelecimento();
+    }, [id]);
+
+    return (
+        <View>
+            <Text>{estabelecimento?.nome}</Text>
+            <TextInput placeholder="Nome do estabelecimento" />
+        </View>
+    );
+};
